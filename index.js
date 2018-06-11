0edcbc9f01af2524c4243035359412bd9fc746df
Azhar Shah <azharshahstl@yahoo.com>
2018-06-11 21:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(HELLO)
+  console.log("HELLO")
+}
+function logWhisper(string) {
+  console.log("hello")
+}
+function sayHiToGrandma(string) {
+  var lowercase = "hello"
+  if lowercase.toLowerCase() === lowercase
+  return
 }
